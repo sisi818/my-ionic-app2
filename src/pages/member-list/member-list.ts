789fa06b303a0095632c26c81a37d51a718d4021
sisi.yuan <stepheny.sisi.yuan@accenture.com>
2017-04-21 17:48:39 +0800

    feat(List) update: data

--- src/pages/member-list/member-list.ts
+++ src/pages/member-list/member-list.ts
@@ -19,13 +19,13 @@ export class MemberList {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public actionCtrl: ActionSheetController) {
     this.members=[
-      {id:1, date:"2017.04.01", weekDay:"Mon", weather:"male", avatar:"assets/img/2-2.png", isHere:true},
-      {id:2, date:"2017.04.02", weekDay:"Tue", weather:"female", avatar:"assets/img/2-0.png", isHere:false},
-      {id:3, date:"2017.04.03", weekDay:"Wen", weather:"male", avatar:"assets/img/2-2.png", isHere:true},
-      {id:4, date:"2017.04.04", weekDay:"Thu", weather:"female", avatar:"assets/img/2-8.png", isHere:false},
-      {id:5,  date:"2017.04.05", weekDay:"Fri", weather:"male", avatar:"assets/img/2-6.png", isHere:true},
-      {id:6, date:"2017.04.06", weekDay:"Sat", weather:"male", avatar:"assets/img/2-4.png", isHere:true},
-      {id:7, date:"2017.04.07", weekDay:"Sun", weather:"male", avatar:"assets/img/1-6.png", isHere:true}
+      {id:1, date:"2017.04.01", weekDay:"Mon", city:"北京", avatar:"assets/img/2-2.png", isHere:true},
+      {id:2, date:"2017.04.02", weekDay:"Tue", city:"上海", avatar:"assets/img/2-0.png", isHere:false},
+      {id:3, date:"2017.04.03", weekDay:"Wen", city:"大连", avatar:"assets/img/2-2.png", isHere:true},
+      {id:4, date:"2017.04.04", weekDay:"Thu", city:"天津", avatar:"assets/img/2-8.png", isHere:false},
+      {id:5,  date:"2017.04.05", weekDay:"Fri", city:"兰州", avatar:"assets/img/2-6.png", isHere:true},
+      {id:6, date:"2017.04.06", weekDay:"Sat", city:"哈尔滨", avatar:"assets/img/2-4.png", isHere:true},
+      {id:7, date:"2017.04.07", weekDay:"Sun", city:"长沙", avatar:"assets/img/1-6.png", isHere:true}
     ];
     this.sortFlag = true;
   }
